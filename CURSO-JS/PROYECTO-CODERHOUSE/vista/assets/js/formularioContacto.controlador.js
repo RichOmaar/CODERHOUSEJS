ae123a9106dc0c4f18b29567ae6e138bf8fce0b8
RichOmaar <39716286+RichOmaar@users.noreply.github.com>
2021-06-28 17:24:05 -0500

    App ARIA react

--- CURSO-JS/PROYECTO-CODERHOUSE/vista/assets/js/formularioContacto.controlador.js
+++
@@ -1,9 +0,0 @@
-class controladorFormularioContacto{
-    async insertarDatosFormulario(datos) {
-        return $.ajax({
-            data:datos,
-            type:"POST",
-            url:"vista/assets/js/ajax/datosFormulario.ajax.php",
-        });
-    }
-}
