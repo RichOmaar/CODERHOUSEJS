eafebdf212e24f7bdaa4b9e824126e06c5778492
RichOmaar <39716286+RichOmaar@users.noreply.github.com>
2021-07-09 15:36:15 -0500

    Botón de cantidad en los productos

--- aria/src/components/ItemListContainer.js
+++ aria/src/components/ItemListContainer.js
@@ -1,11 +1,15 @@
-import { Card,Button,Col } from 'react-bootstrap';
+import { useState } from 'react';
+import { Card,Button,Col,Form,Row } from 'react-bootstrap';
 
 export default function ItemListContainer(props) {
+
+    const [qty,setQty] = useState(1);
+
     return (
         <Col style={{
             paddingTop: '50px'
         }}>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '18rem' }} id={props.id}>
             <Card.Img variant="top" src={props.img} />
             <Card.Body>
                 <Card.Title>{props.titulo}</Card.Title>
@@ -15,7 +19,37 @@ export default function ItemListContainer(props) {
                 <Card.Text>
                     <p style={{fontWeight: 'bold'}}>${props.precio} MXN</p>
                 </Card.Text>
-                <Button variant="primary">Comprar</Button>
+                <Form onSubmit={ (data) => {props.addToCart(data,props)} }>
+                    <Form.Group className="itemQty">
+                    {/* <Form.Group className="itemQty"> */}
+                    <Row>
+                        <Col>
+                            <Button variant="secondary" type="button" onClick={ () => {
+                                if(props.stock > qty) {
+                                    setQty(qty + 1)
+                                }
+                            }}>
+                                +
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Form.Control type="number"  value={qty} />
+                        </Col>
+                        <Col>
+                            <Button variant="secondary"  type="button" onClick={ () => {
+                                if(qty > 1){
+                                    setQty(qty - 1);
+                                }
+                            }}>
+                                -
+                            </Button>
+                        </Col>
+                    </Row>
+                    </Form.Group>
+                    <Button className="mt-4 btn-block" variant="primary" type="submit">
+                        Comprar
+                    </Button>
+                </Form>
             </Card.Body>
             </Card>
         </Col>
--- aria/src/components/Items.js
+++ aria/src/components/Items.js
@@ -1,65 +1,115 @@
-import { Component } from 'react';
-import { Row,Container } from 'react-bootstrap';
+import { useState,useEffect,useRef } from 'react';
+import { Row,Container,Card } from 'react-bootstrap';
 import ItemListContainer from "./ItemListContainer";
 
-export default class Items extends Component {
-    constructor(props) {
-        super(props);
-        this.array = [
-            {
-                id:1,
-                img:"https://ariamor.com.mx/vista/assets/images/soap-product-1.jpg",
-                titulo:"Tea Totaler",
-                descripcion:"Para una piel limpia y sin impurezas",
-                precio:200
-            },
-            {
-                id:2,
-                img:"https://ariamor.com.mx/vista/assets/images/soap-product-2.jpg",
-                titulo:"Jason And The Argan Oil",
-                descripcion:"Una melena sedosa y con aroma a rosas",
-                precio:255
-            },
-            {
-                id:3,
-                img:"https://ariamor.com.mx/vista/assets/images/soap-product-3.jpg",
-                titulo:"Aromaco",
-                descripcion:"Refrescante y efectivo",
-                precio:175
-            },
-            {
-                id:3,
-                img:"https://ariamor.com.mx/vista/assets/images/soap-product-4.jpg",
-                titulo:"Outback Mate",
-                descripcion:"El que quiere azul celeste, que Outback Mate pruebe",
-                precio:170
-            }
-        ];
+export default function Items() {
+
+    const [cart,setCart] = useState([]);
+    const [array,setArray] = useState([
+                {
+                    id:1,
+                    img:"https://ariamor.com.mx/vista/assets/images/soap-product-1.jpg",
+                    titulo:"Tea Totaler",
+                    descripcion:"Para una piel limpia y sin impurezas",
+                    precio:200,
+                    stock: 20
+                },
+                {
+                    id:2,
+                    img:"https://ariamor.com.mx/vista/assets/images/soap-product-2.jpg",
+                    titulo:"Jason And The Argan Oil",
+                    descripcion:"Una melena sedosa y con aroma a rosas",
+                    precio:255,
+                    stock: 2
+                },
+                {
+                    id:3,
+                    img:"https://ariamor.com.mx/vista/assets/images/soap-product-3.jpg",
+                    titulo:"Aromaco",
+                    descripcion:"Refrescante y efectivo",
+                    precio:175,
+                    stock: 1
+                },
+                {
+                    id:3,
+                    img:"https://ariamor.com.mx/vista/assets/images/soap-product-4.jpg",
+                    titulo:"Outback Mate",
+                    descripcion:"El que quiere azul celeste, que Outback Mate pruebe",
+                    precio:170,
+                    stock: 50
+                }
+            ]
+    );
+
+    const items = () => {
+        let item = array.map(index => {
+            return (
+                <ItemListContainer
+                    id = {index.id}
+                    img = {index.img}
+                    titulo = {index.titulo}
+                    descripcion = {index.descripcion}
+                    precio = {index.precio}
+                    stock = {index.stock}
+                    addToCart = {getProducts}
+                    /> 
+            )
+        })
+
+        return item;
     }
 
-    render() {
-        const mapFunctionComponente = () => {
-            let items = [];
+    const primerRender = useRef(true);
 
-            for(let index of this.array){
-                let item = <ItemListContainer
-                            id = {index.id}
-                            img = {index.img}
-                            titulo = {index.titulo}
-                            descripcion = {index.descripcion}
-                            precio = {index.precio}
-                            />
-                    items = [...items,item];
-            }
-            return items;
+    const getProducts = (form,item) => {
+        form.preventDefault();
+        // console.log(form);
+        // console.log(item);
+        let items = {
+            titulo:item.titulo,
+            img:item.img,
+            precio:item.precio,
+            qty:form.target[1].value
         }
 
-        return (
-            <Container>
-                <Row>
-                    {mapFunctionComponente()}
-                </Row>
-            </Container>
-        )
+        setCart([...cart,items]);
+        // console.log(form.target[1].value);
+        console.log(cart);
     }
+
+    useEffect(() => {
+        if(primerRender.current){
+            primerRender.current = false;
+        } else{
+            alert('Se agregó el item al carrito');
+        }
+    },[cart]);
+    
+
+    return (
+        <Container>
+            <Row>
+                {items()}
+                {/* <ItemListContainer
+                    id= '1'
+                    img= "https://ariamor.com.mx/vista/assets/images/soap-product-1.jpg"
+                    titulo= "Tea Totaler"
+                    descripcion= "Para una piel limpia y sin impurezas"
+                    precio= '200'
+                    stock=  '20'
+                    addToCart = {getProducts}
+                    />   */}
+
+                {/* <Card.Text className="subtotal1">
+                    {cart.map(element => {
+                        return (
+                            <>
+                                <h1>Hola mundo</h1>
+                            </>
+                        );
+                    })}
+                </Card.Text> */}
+            </Row>
+        </Container>
+    )
 }
